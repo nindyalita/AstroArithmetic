7086aec000ee2cf1d391ee6435f35afdae3e8ce0
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-08-21 19:05:49 +0700

    update winningscreen

--- MCOne/Views/WinningScreen.swift
+++ MCOne/Views/WinningScreen.swift
@@ -8,40 +8,38 @@
 import SwiftUI
 
 struct WinningScreen: View {
+    
     @StateObject private var winningViewModel = WinningViewModel()
     
     var body: some View {
-        VStack {
-            NavigationView{
-                Image("helmet3")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300)
-                    .padding(.top, 40)
-                
-                NavigationLink(destination: WelcomeScreen()) {
-                    Button {
-                        winningViewModel.replayGame.toggle()
-                    } label: {
-                        Image("replayGameButton")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 150)
-                    }
-                    .padding(.vertical, 10)
-                }
-            }
-            .position(x: 380, y: 210)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                Image("BgWinningScreen")
-                    .resizable()
-                    .edgesIgnoringSafeArea(.all)
-            )
-            .navigationBarHidden(true)
+        VStack{
+            Image("helmet3")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300)
+                .padding(.top, 40)
             
+            NavigationLink(destination: WelcomeScreen(), isActive: $winningViewModel.replayGame){
+                Button(){
+                    print("correct")
+                    winningViewModel.replayGame.toggle()
+                } label: {
+                    Image("replayGameButton")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150)
+                }
+                .padding(.vertical, 10)
+            }
         }
-        
+        .position(x:380, y: 210)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("BgWinningScreen")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+        )
+        .navigationBarHidden(true)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
